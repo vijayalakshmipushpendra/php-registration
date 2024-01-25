d3178ef9731c3b0e8f21ba7d3bd3f4b00674d4d0
vijayalakshmipushpendra <146848558+vijayalakshmipushpendra@users.noreply.github.com>
2024-01-25 22:01:41 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Database configuration
-define('DB_HOST', 'database-1.cjugeuoq3svl.ap-south-1.rds.amazonaws.com');
-define('DB_USER', 'admin');
-define('DB_PASS', 'admin123');
-define('DB_NAME', 'register');
+define('DB_HOST', 'endpoint of database');
+define('DB_USER', 'username for database');
+define('DB_PASS', 'password for database');
+define('DB_NAME', 'name of database');
 ?>
